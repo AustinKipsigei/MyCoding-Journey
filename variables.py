34f60d31c59a4946cf3de9ad724d7bcc0a59aaf4
AustinKipsigei <austin.kipsigei@gmail.com>
2025-03-07 11:33:12 +0300

    variable.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#This program shows how to declare variables 
+#In this program we will declare "meal" as a variable and asign it a value and print the result
+meal="An English Muffin"
+print("Breakfast:"+ meal)
+
+#How about Lunch
+meal="Rice and Chicken"
+print("Lunch:"+ meal)
+
+#How about dinner
+
+meal="Pizza"
+print("Dinner:"+ meal)
